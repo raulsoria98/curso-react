39db46c1c97b82f70e6e0d6e8ac8e44e26446592
Ray <raulsogo@hotmail.com>
2023-03-13 18:02:26 +0100

    No hace la misma búsqueda dos veces seguidas (useRef)

--- projects/05-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-buscador-peliculas/src/hooks/useMovies.js
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
   const [, setError] = useState(null)
   const [loading, setLoading] = useState(false)
+  const previousSearch = useRef(search)
 
   const getMovies = async () => {
+    if (previousSearch.current === search) return
+
     try {
       setLoading(true)
       setError(null)
+      previousSearch.current = search
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch (error) {
